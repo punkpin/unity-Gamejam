bcec11be941c4056b94e03b3b161b6c1ed10a671
大懒猫 <3354882782@qq.com>
2024-12-24 21:02:55 +0800

    The Tetris function is realized, the enemy module is realized, and the player movement and jump function is realized.

---
+++ Assets/ZombiSoft/TinyHealthSystem/HealthSystem.cs
@@ -0,0 +1,177 @@
+﻿
+using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class HealthSystem : MonoBehaviour
+{
+	public static HealthSystem Instance;
+
+	public Image currentHealthBar;
+	public Image currentHealthGlobe;
+	public TextMeshProUGUI healthText;
+	public float hitPoint = 100f;
+	public float maxHitPoint = 100f;
+
+	public Image currentManaBar;
+	public Image currentManaGlobe;
+	public Text manaText;
+	public float manaPoint = 100f;
+	public float maxManaPoint = 100f;
+
+	public bool Regenerate = true;
+	public float regen = 0.1f;
+	private float timeleft = 0.0f;	
+	public float regenUpdateInterval = 1f;
+
+	public bool GodMode;
+
+	void Awake()
+	{
+		Instance = this;
+	}
+	
+  	void Start()
+	{
+		UpdateGraphics();
+		timeleft = regenUpdateInterval; 
+	}
+
+	void Update ()
+	{
+		if (Regenerate)
+			Regen();
+	}
+
+	private void Regen()
+	{
+		timeleft -= Time.deltaTime;
+
+		if (timeleft <= 0.0) 
+		{
+			// Debug mode
+			if (GodMode)
+			{
+				HealDamage(maxHitPoint);
+				RestoreMana(maxManaPoint);
+			}
+			else
+			{
+				HealDamage(regen);
+				RestoreMana(regen);				
+			}
+
+			UpdateGraphics();
+
+			timeleft = regenUpdateInterval;
+		}
+	}
+
+	private void UpdateHealthBar()
+	{
+		float ratio = hitPoint / maxHitPoint;
+		currentHealthBar.rectTransform.localPosition = new Vector3(currentHealthBar.rectTransform.rect.width * ratio - currentHealthBar.rectTransform.rect.width, 0, 0);
+		healthText.text = hitPoint.ToString ("0") + "/" + maxHitPoint.ToString ("0");
+	}
+
+	private void UpdateHealthGlobe()
+	{
+		float ratio = hitPoint / maxHitPoint;
+		currentHealthGlobe.rectTransform.localPosition = new Vector3(0, currentHealthGlobe.rectTransform.rect.height * ratio - currentHealthGlobe.rectTransform.rect.height, 0);
+		healthText.text = hitPoint.ToString("0") + "/" + maxHitPoint.ToString("0");
+	}
+
+	public void TakeDamage(float Damage)
+	{
+		hitPoint -= Damage;
+		if (hitPoint < 1)
+			hitPoint = 0;
+
+		UpdateGraphics();
+
+		StartCoroutine(PlayerHurts());
+	}
+
+	public void HealDamage(float Heal)
+	{
+		//hitPoint += Heal;
+		//if (hitPoint > maxHitPoint) 
+		//	hitPoint = maxHitPoint;
+
+		//UpdateGraphics();
+	}
+	public void SetMaxHealth(float max)
+	{
+		maxHitPoint += (int)(maxHitPoint * max / 100);
+
+		UpdateGraphics();
+	}
+
+	private void UpdateManaBar()
+	{
+		float ratio = manaPoint / maxManaPoint;
+		currentManaBar.rectTransform.localPosition = new Vector3(currentManaBar.rectTransform.rect.width * ratio - currentManaBar.rectTransform.rect.width, 0, 0);
+		manaText.text = manaPoint.ToString ("0") + "/" + maxManaPoint.ToString ("0");
+	}
+
+	private void UpdateManaGlobe()
+	{
+		float ratio = manaPoint / maxManaPoint;
+		currentManaGlobe.rectTransform.localPosition = new Vector3(0, currentManaGlobe.rectTransform.rect.height * ratio - currentManaGlobe.rectTransform.rect.height, 0);
+		manaText.text = manaPoint.ToString("0") + "/" + maxManaPoint.ToString("0");
+	}
+
+	public void UseMana(float Mana)
+	{
+		manaPoint -= Mana;
+		if (manaPoint < 1) 
+			manaPoint = 0;
+
+		UpdateGraphics();
+	}
+
+	public void RestoreMana(float Mana)
+	{
+		manaPoint += Mana;
+		if (manaPoint > maxManaPoint) 
+			manaPoint = maxManaPoint;
+
+		UpdateGraphics();
+	}
+	public void SetMaxMana(float max)
+	{
+		maxManaPoint += (int)(maxManaPoint * max / 100);
+		
+		UpdateGraphics();
+	}
+
+	private void UpdateGraphics()
+	{
+		UpdateHealthGlobe();
+
+	}
+
+	IEnumerator PlayerHurts()
+	{
+		
+		PopupText.Instance.Popup("Ouch!", 1f, 1f); 
+
+		if (hitPoint < 1) // Health is Zero!!
+		{
+			yield return StartCoroutine(PlayerDied()); 
+		}
+
+		else
+			yield return null;
+	}
+
+	IEnumerator PlayerDied()
+	{
+		// Player is dead. Do stuff.. play anim, sound..
+		PopupText.Instance.Popup("You have died!", 1f, 1f); // Demo stuff!
+
+		yield return null;
+	}
+}
