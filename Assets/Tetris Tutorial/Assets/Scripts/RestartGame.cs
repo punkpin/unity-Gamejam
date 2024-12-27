5aee6628459eb0130e5b59cea5a3e9d27c1b8c81
大懒猫 <3354882782@qq.com>
2024-12-27 09:53:58 +0800

    Added some updates

--- Assets/Tetris Tutorial/Assets/Scripts/RestartGame.cs
+++ Assets/Tetris Tutorial/Assets/Scripts/RestartGame.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
-
+using TMPro;
+using UnityEngine.SocialPlatforms.Impl;
 public class RestartGame : MonoBehaviour
 {
     [Header("Scripts")]
@@ -11,14 +12,19 @@ public class RestartGame : MonoBehaviour
     [SerializeField] public Enemy_Factory enemy;
     [SerializeField] public GameObject[] Start_Prefabs;
     [SerializeField] public GameObject Starts;
+    [SerializeField] public TextMeshProUGUI score_text;
+    [SerializeField] public TextMeshProUGUI local_score_text;
+    [SerializeField] private int local_text;
     [Header("GameObject")]
     [SerializeField] public GameObject Enemys;
+    [SerializeField] public GameObject Finish;
+    [SerializeField] public GameObject Player;
     private Vector3 player_position = new Vector3(3.87899995f, 2.54060006f, 0);
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        local_text = PlayerPrefs.GetInt("HighScore", 0);
     }
 
     // Update is called once per frame
@@ -26,13 +32,19 @@ public class RestartGame : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.R))
         {
-            RestartGame_();
+            GameConst.PLAYER_IS_DEAD = true;
+            Finish_Game();
+        }
+        if (GameConst.PLAYER_IS_DEAD)
+        {
+            Finish_Game();
         }
     }
 
     public void RestartGame_()
     {
-       
+        Time.timeScale = 1f;
+        GameConst.PLAYER_IS_DEAD = false;
         timer.elapsedTime = 0f;
         ScoreManager.Score = 0;
         foreach (Transform child in GameObject.Find("Blocks").transform)
@@ -51,9 +63,11 @@ public class RestartGame : MonoBehaviour
             Destroy(child.gameObject);
         }
         FindObjectOfType<SpawnTetromino>().NewTetromino();
-        GameObject.Find("Player").transform.position = player_position;
+        Instantiate(Player);
+        Player.transform.position = player_position;
         enemy.Clear_Level();
         HealthSystem.Instance.hitPoint = 10f;
+        Finish.SetActive(false);
     }
     public void StartGame()
     {
@@ -62,6 +76,22 @@ public class RestartGame : MonoBehaviour
         {
             Start_Prefabs[i].SetActive(true);
         }
-        
+        Instantiate(Player);
+        Player.transform.position = player_position;
+
+    }
+    public void Finish_Game()
+    {
+        Finish.SetActive(true);
+        Time.timeScale = 0f;
+        score_text.text = ScoreManager.Score.ToString();
+        if (local_text < ScoreManager.Score)
+        {
+            local_text = ScoreManager.Score;
+            PlayerPrefs.SetInt("HighScore", local_text);  // 存储分数，键值为 "HighScore"
+            PlayerPrefs.Save();  // 强制保存到磁盘
+        }
+        local_score_text.text = local_text.ToString();
+
     }
 }
